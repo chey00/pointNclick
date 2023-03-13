236522366e231e28fc04fb8f83e353ea880c596a
student <hannes.weber2001@gmail.com>
2023-03-13 09:03:40 +0100

    Added office of Frau Vogel

--- BueroVogel.py
+++ BueroVogel.py
@@ -43,16 +43,16 @@ class BueroVogel(TemplateRoom):
             self.text_line_4 = ""
             self.text_line_5 = ""
         elif self.hitbox_raumzuweisung.contains(mouse_pos):
-            self.text_line_1 = "Hier ist Die Raumzuweisung: EG-105"
-            self.text_line_2 = ""
-            self.text_line_3 = "Diese Schilder sind an jedem Raum,"
-            self.text_line_4 = ""
-            self.text_line_5 = "um Missverständnisse zu vermeiden."
+            self.text_line_1 = "Hier ist Die Raumzuweisung:"
+            self.text_line_2 = "EG - 105"
+            self.text_line_3 = "Diese Schilder sind"
+            self.text_line_4 = "an jedem Raum, um "
+            self.text_line_5 = "Missverständnisse zu vermeiden."
         elif self.hitbox_acces.contains(mouse_pos):
-            self.text_line_1 = "Hier ist der Acces point zu sehen."
-            self.text_line_2 = "Er sorgt für eine stabile"
-            self.text_line_3 = "Internetverbindung und "
-            self.text_line_4 = "ist im ganzen Schulhaus zu finden."
-            self.text_line_5 = ""
+            self.text_line_1 = "Hier ist der Acces point zu"
+            self.text_line_2 = "sehen. Er sorgt"
+            self.text_line_3 = "für eine stabile"
+            self.text_line_4 = "Internetverbindung und ist im"
+            self.text_line_5 = "ganzen Schulhaus zu finden."
 
         self.update()
