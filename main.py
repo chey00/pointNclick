0c6ed94726abf67e5f86609f68985f2533b50e7a
chey00 <c.hey@sbs-herzogenaurach.de>
2023-04-17 12:20:55 +0200

    Added Splash Screen.

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtCore import Qt, QThread
+from PyQt6.QtGui import QPixmap, QColor
+from PyQt6.QtWidgets import QApplication, QSplashScreen
 
 from MainWindow import MainWindow
 # All you need is
 # https://doc.qt.io/qtforpython/
 
 app = QApplication(sys.argv)
+
+splash = QSplashScreen(QPixmap("logo.png"))
+splash.show()
+
+app.processEvents()
+#QThread.sleep(2)
+
 main_window = MainWindow()
+splash.finish(main_window)
 main_window.show()
 sys.exit(app.exec())
