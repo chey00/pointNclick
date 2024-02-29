d1422271effd6312a06c9242efc9f1f5088c4b7d
Christian Hey <c.hey@sbs-herzogenaurach.de>
2024-02-29 13:30:19 +0100

    Clean up main.py Added two pictures for new rooms

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 
-from PyQt6.QtCore import Qt, QThread
-from PyQt6.QtGui import QPixmap, QColor
+from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QApplication, QSplashScreen
 
 from MainWindow import MainWindow
@@ -14,7 +13,6 @@ splash = QSplashScreen(QPixmap("logo.png"))
 splash.show()
 
 app.processEvents()
-#QThread.sleep(2)
 
 main_window = MainWindow()
 splash.finish(main_window)
