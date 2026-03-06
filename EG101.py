97e6cc6e5953227963afd0ef1d1ca544938c59dc
Student <michael.wehr@sbs-herzogenaurach.de>
2026-03-06 10:05:56 +0100

    neues Bild, mit boxen und Textänderung

--- EG101.py
+++ EG101.py
@@ -10,33 +10,33 @@ class EG101(TemplateRoom):
 
         self.init_room("EG101.jpg")
 
-        self.offset_balloon_x = 650
-        self.offset_balloon_y = 270
-        self.offset_balloon_length = 570
+        self.offset_balloon_x = 500
+        self.offset_balloon_y = 140
+        self.offset_balloon_length = 630
         self.offset_balloon_width = 170
 
-        self.set_offset_mouth(600, 530, 0, 100)
+        self.set_offset_mouth(971, 392, 0, 100)
 
         self.__counter = 0
 
-        self.hitbox_PC = QRect(480, 695, 240, 110)
+        self.hitbox_PC = QRect(1034, 572, 240, 68)
         self.append_hitbox(self.hitbox_PC)
 
-        self.hitbox_ordner = QRect(1, 710, 207, 180)
-        self.append_hitbox(self.hitbox_ordner)
+        self.hitbox_tasche = QRect(50, 658, 290, 160)
+        self.append_hitbox(self.hitbox_tasche)
 
-        self.hitbox_tafel = QRect(900, 470, 300, 200)
+        self.hitbox_sparschwein = QRect(879, 586, 63, 50)
+        self.append_hitbox(self.hitbox_sparschwein)
+
+        self.hitbox_tafel = QRect(1168, 174, 300, 340)
         self.append_hitbox(self.hitbox_tafel)
 
-        self.hitbox_stundenplan = QRect(400, 340, 230, 125)
-        self.append_hitbox(self.hitbox_stundenplan)
-
         self.text_line_1 = "Hallo!"
-        self.text_line_2 = "Ich bin Herr Gumbmann und unterrichte"
-        self.text_line_3 = "das Fach Programmieren. Willkommen im"
-        self.text_line_4 = "Raum EG101. Hier ist die Klasse FSWI-1,"
-        self.text_line_5 = "die den Schulhausrundgang aktualisiert"
-        self.text_line_6 = "hat."
+        self.text_line_2 = "Ich bin Herr Hey und unterrichte"
+        self.text_line_3 = "das Fach Programmieren. Wenn du gut"
+        self.text_line_4 = "aufpasst, verstehst du nicht nur den Stoff"
+        self.text_line_5 = "besser - vielleicht gibt es sogar eine"
+        self.text_line_6 = "bessere Note. Also Ohren auf, Gehirn an!"
 
     def mousePressEvent(self, ev: QMouseEvent) -> None:
         super(EG101, self).mousePressEvent(ev)
@@ -51,7 +51,7 @@ class EG101(TemplateRoom):
             self.text_line_5 = "dazu noch ein MacBook Air."
             self.text_line_6 = ""
 
-        elif self.hitbox_ordner.contains(mouse_pos):
+        elif self.hitbox_tasche.contains(mouse_pos):
             self.text_line_1 = ""
             self.text_line_2 = "Wir schreiben mindestens zwei"
             self.text_line_3 = "Leistungsnachweise pro Halbjahr:"
@@ -59,15 +59,15 @@ class EG101(TemplateRoom):
             self.text_line_5 = "Schulaufgabe."
             self.text_line_6 = ""
 
-        elif self.hitbox_tafel.contains(mouse_pos):
-            self.text_line_1 = "Im ersten Schuljahr,"
-            self.text_line_2 = "haben Wirtschaftsinformatiker"
-            self.text_line_3 = "eine 37-Stunden-Woche."
-            self.text_line_4 = "Parallel zum Bachelor, können Sie"
-            self.text_line_5 = "das Fachabi durch eine "
-            self.text_line_6 = "Ergänzungsprüfung in Englisch erwerben."
+        elif self.hitbox_sparschwein.contains(mouse_pos):
+            self.text_line_1 = "Dieses Sparschwein ist"
+            self.text_line_2 = "ein kleiner BWL-Experte."
+            self.text_line_3 = "Als Wirtschaftsinformatiker ist."
+            self.text_line_4 = "Betriebswirtschaft auch ein wichtiger"
+            self.text_line_5 = "Fachbereich. In der Betriebswirtschaft "
+            self.text_line_6 = "lernt man, wie man mit Geld richtig umgeht!"
 
-        elif self.hitbox_stundenplan.contains(mouse_pos):
+        elif self.hitbox_tafel.contains(mouse_pos):
             self.text_line_1 = "Lehrplan:"
             self.text_line_2 = "Deutsch, Englisch, Sozialkunde,"
             self.text_line_3 = "Programmieren, Betriebswirtschaft,"
