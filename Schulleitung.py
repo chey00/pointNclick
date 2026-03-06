821503e8509be4251f2912877a3643b962296245
student <reza.herzii@gmail.com>
2026-03-06 09:01:39 +0100

    Raum für neue Schulleiterin angepasst.

--- Schulleitung.py
+++ Schulleitung.py
@@ -10,27 +10,27 @@ class Schulleitung(TemplateRoom):
 
         self.init_room("Schulleitung.jpg")
 
-        self.offset_balloon_x = 515
+        self.offset_balloon_x = 586
         self.offset_balloon_y = 150
         self.offset_balloon_length = 550
         self.offset_balloon_width = 165
-        self.set_offset_mouth(563, 421, 100, 200)
+        self.set_offset_mouth(671, 442, 100, 200)
 
-        self.hitbox_mouth = QRect(935, 280, 100, 30)
+        self.hitbox_mouth = QRect(1005, 280, 100, 30)
         self.append_hitbox(self.hitbox_mouth)
 
-        self.hitbox_door = QRect(0, -100, 140, 1000)
+        self.hitbox_door = QRect(975, 725, 300, 125)
         self.append_hitbox(self.hitbox_door)
 
-        self.hitbox_schulregeln = QRect(161, 195, 190, 200)
+        self.hitbox_schulregeln = QRect(25, 75, 425, 400)
         self.append_hitbox(self.hitbox_schulregeln)
 
-        self.hitbox_easter_egg = QRect(750, 535, 80, 80)
+        self.hitbox_easter_egg = QRect(569, 650, 80, 80)
 
         self.text_line_1 = "Herzlich Willkommen!"
         self.text_line_2 = ""
-        self.text_line_3 = "Mein Name ist Hr. Wirsching."
-        self.text_line_4 = "Ich bin hier der Schulleiter."
+        self.text_line_3 = "Mein Name ist Frau Wirzberger-Camacho."
+        self.text_line_4 = "Ich bin hier die Schulleiterin."
         self.text_line_5 = "Schön, dass Sie hier sind!"
         self.text_line_6 = "                             Weiter"
 
